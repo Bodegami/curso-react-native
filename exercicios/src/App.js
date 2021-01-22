734e873e0a3a61cf7693d614c4a16bd0f69d72e0
Renato <rena.f.oliveira@gmail.com>
2021-01-22 19:34:30 -0300

    Exercicio - destructuring && props

--- exercicios/src/App.js
+++ exercicios/src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
 
+import Aleatorio from './components/Aleatorios'
 import MinMax from './components/MinMax';
 import CompPadrao, { Comp2, Comp3 } from './components/Multi';
 import Primeiro from'./components/Primeiro';
 
 export default () => (
   <View style={style.App}>
-    <MinMax min={3} max={20}/>
+    <Aleatorio min={1} max={60}/>
+    {/* <MinMax min={3} max={20}/>
     <MinMax min={1} max={94}/>
-    {/* <CompPadrao />
+    <CompPadrao />
     <Comp2 />
     <Comp3 />
     <Primeiro /> */}
