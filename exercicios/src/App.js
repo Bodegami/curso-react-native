5f98197a2a820e29f13df0170f8d2efacd8b9e8f
Renato <rena.f.oliveira@gmail.com>
2021-01-22 11:53:20 -0300

    Sobre StyleSheet

--- exercicios/src/App.js
+++ exercicios/src/App.js
@@ -1,16 +1,25 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, StyleSheet} from 'react-native';
 
 import CompPadrao, { Comp2, Comp3 } from './components/Multi';
 import Primeiro from'./components/Primeiro';
 
 export default () => (
-  <View>
+  <View style={style.App}>
     <CompPadrao />
     <Comp2 />
     <Comp3 />
     <Primeiro />
   </View>
 )
+
+const style = StyleSheet.create({
+  App: {
+    flexGrow: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+
+  }
+})
 
 
--- exercicios/src/components/Multi.js
+++ exercicios/src/components/Multi.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Text } from 'react-native';
+import Estilo from './estilo';
 
 export default function Comp1() {
-  return <Text>Comp #01 Oficial</Text>
+  return <Text style={Estilo.fontG}>Comp #01 Oficial</Text>
 }
 
 export function Comp2() {
-  return <Text>Comp #02</Text>
+  return <Text style={Estilo.fontG}>Comp #02</Text>
 }
 
 export function Comp3() {
-  return <Text>Comp #03</Text>
+  return <Text style={Estilo.fontG}>Comp #03</Text>
 }
