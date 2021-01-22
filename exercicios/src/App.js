6a066013b2f9afc6af2e619075828ffe984ede9d
Renato <rena.f.oliveira@gmail.com>
2021-01-22 11:27:39 -0300

    Modulos import/ export

--- exercicios/src/App.js
+++ exercicios/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import {View} from 'react-native';
 
+import CompPadrao, { Comp2, Comp3 } from './components/Multi';
 import Primeiro from'./components/Primeiro';
 
 export default () => (
   <View>
-      <Primeiro />
-    </View>
+    <CompPadrao />
+    <Comp2 />
+    <Comp3 />
+    <Primeiro />
+  </View>
 )
 
 
